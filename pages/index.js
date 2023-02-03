15a7c029986cdd07fc29cd35b1ec723749a7a77a
cartervandekamp <113644516+cartervandekamp@users.noreply.github.com>
2023-02-02 17:42:10 -0800

    devices

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className={styles.container} id="colouring">
+      <nav>
+        <Link href="/about">About</Link>
+      </nav>
+    </main>
+  );
+}
